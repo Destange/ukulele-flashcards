14174e1ec5bf9c66e82f1d4ea5d29510234bfb3c
Destange <juliendestange@gmail.com>
2026-05-24 05:20:10 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+var CACHE = 'ukulele-v3.0';
+var ASSETS = ['./', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  // Ne pas intercepter les requêtes CDN externes (audio, polices)
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(resp) {
+        var clone = resp.clone();
+        caches.open(CACHE).then(function(c) { c.put(e.request, clone); });
+        return resp;
+      });
+    })
+  );
+});
