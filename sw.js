9ad6417c3268a9db258b238abd1f083f0c90844e
Destange <juliendestange@gmail.com>
2026-05-25 03:38:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'ukulele-v3.22';
+var CACHE = 'ukulele-v3.23';
 var ASSETS = ['./', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
